37a7e0956664499d6d342195ec63edcb6514c92c
chinmayz <chinmay1gupta@gmail.com>
2013-08-07 18:54:56 +0000

    Edited prod desc in index.html

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ response.send(fname.toString('utf8'));
 
 }); 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
